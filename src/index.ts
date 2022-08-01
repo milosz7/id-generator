a9291a5dfb5c5ed30b0db46fd28a44f16867f508
Milosz B <miloszbargiel@gmail.com>
2022-08-01 20:23:02 +0200

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
   ? Acc[number]
-  : Enumerate<N, [...Acc, Acc['length']]>
+  : Enumerate<N, [...Acc, Acc['length']]>;
 
-type Range<Min extends number, Max extends number> = Exclude<Enumerate<Max>, Enumerate<Min>>
+type Range<Min extends number, Max extends number> = Exclude<Enumerate<Max>, Enumerate<Min>>;
 
 const minIdLength = 5;
 const maxIdLength = 10;
 const generatedIds: string[] = [];
 
-
 export const randomId = (idLength: Range<5, 11>) => {
   if (typeof idLength !== 'number') throw Error('Provided argument is not a number!');
-  const generatedIdLength = idLength < minIdLength ? minIdLength : idLength <= maxIdLength ? idLength : maxIdLength; 
+  const generatedIdLength = idLength < minIdLength ? minIdLength : idLength <= maxIdLength ? idLength : maxIdLength;
   let id = '';
   const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!_@#$%^&';
   const charactersAmount = possibleCharacters.length;

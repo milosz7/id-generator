c691c3acdcc7eff021e2ef0557ddb94155ea4979
Milosz B <miloszbargiel@gmail.com>
2022-08-01 19:45:29 +0200

    Add package source code

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>
+
+type Range<Min extends number, Max extends number> = Exclude<Enumerate<Max>, Enumerate<Min>>
+
+const minIdLength = 5;
+const maxIdLength = 10;
+const generatedIds: string[] = [];
+
+
+export const randomId = (idLength: Range<5, 11>) => {
+  const generatedIdLength = idLength < minIdLength ? minIdLength : idLength <= maxIdLength ? idLength : maxIdLength; 
+  let id = '';
+  const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!_@#$%^&';
+  const charactersAmount = possibleCharacters.length;
+  for (let i = 0; i < generatedIdLength; i++) {
+    const randomCharacter = possibleCharacters[Math.floor(Math.random() * charactersAmount)];
+    id += randomCharacter;
+  }
+  if (generatedIds.indexOf(id) === -1) {
+    generatedIds.push(id);
+  } else {
+    randomId(idLength);
+  }
+  return id;
+};
